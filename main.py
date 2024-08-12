ba2da89c2f6cd5e6919a35a99798f5d8ce99e937
Natalie Leung <mmoopal@gmail.com>
2024-08-12 10:15:20 -0700

    Added styling. Can upload videos and display ordered thumbnails and select them

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 import eel, os, random
 import hachoir
+import cv2
+import io, base64
 from hachoir.parser import createParser
 from hachoir.metadata import extractMetadata
 from tkinter import filedialog
 from tkinter import *
+from PIL import Image
+
 
 eel.init('web')
+clips = []
+class Clip:
+    def __init__(self, path, date, thumbnail):
+        self.path = path
+        self.date = date
+        self.thumbnail = thumbnail
+    def __lt__(self, other):
+        return self.date < other 
+    def __gt__(self, other):
+        return self.date > other
+    def __eq__(self, other):
+        return self.date == other
 @eel.expose
 def get_path():
     root = Tk()
     root.withdraw()
     root.wm_attributes('-topmost',1)
     
-    root.filename = filedialog.askopenfilename()
+    root.filenames = filedialog.askopenfilenames()
     root.update()
-    print("heyyyy")
-    print(root.filename)
-    extract_metadata(root.filename)
+    file_list = list(root.filenames)
+    for file_path in file_list:
+        date = extract_metadata(file_path)
+        thumbnail = extract_thumbnail(file_path)
+        clips.append(Clip(file_path, date, thumbnail))
+        
+        
+        #TODO handle when metadata cannot be extracted
+    #print(root.filename)
+    #extract_metadata(root.filename)
 
 def extract_metadata(file_path):
     # Open the file
@@ -27,15 +50,45 @@ def extract_metadata(file_path):
         metadata = extractMetadata(parser)
         if not metadata:
             print("Unable to extract metadata")
+            return None
         else:
             print(metadata.get('creation_date'))
-        
+            return metadata.get('creation_date')
+
+def extract_thumbnail(file_path):
+    cap = cv2.VideoCapture(file_path)
+    ret, frame = cap.read()
+    if ret:
+        img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        buffer = io.BytesIO()
+        img.save(buffer, format='JPEG')
+        encoded_img = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        return f'data:image/jpeg;base64,{encoded_img}'
+    return None
+
 @eel.expose
-def pick_file(folder):
-    if os.path.isdir(folder):
-        return random.choice(os.listdir(folder))
-    else:
-        return 'Not valid folder'
+def get_thumbnails():
+    clips.sort()
+    return [clip.thumbnail for clip in clips]
+
+#maybe send over list of clip objects? for javascript to handle with 
+#search up matieral ui but for javascript
+#1. try to display thumbnail after uploading it 
+#2. display ORDERED thumbnails
+#3 do groups logic:
+"""
+- side bar with groups
+    create group button
+    groups have a text input for name
+    list
+-  at least one video must be selected in order to create a group
+- different colors
+- get added to video class?
+- maybe later: be able to play the video, or maybe just get another frame???
+"""
+
+#create videos:
+
 
 
 eel.start('index.html', size=(400, 400))
--- web/script.js
+++ web/script.js
@@ -1,9 +1,22 @@
-const fileSelect = $('.select-file');
-    fileSelect.click(function()
-    {
-        eel.get_path();
-    }
-)
-function test(){
-    eel.get_path()
+function get_path(){
+    eel.get_path()(loadThumbnails)
+}
+async function loadThumbnails() {
+    let thumbnails = await eel.get_thumbnails()();
+    let container = document.getElementById('thumbnails');
+    container.innerHTML = '';
+    thumbnails.forEach((thumb, index) => {
+        if (thumb) {
+            let img = document.createElement('img');
+            img.src = thumb;
+            img.className = 'thumbnail';
+            img.dataset.index = index;
+            img.onclick = () => toggleSelection(img);
+            container.appendChild(img);
+        }
+    });
+}
+
+function toggleSelection(item){
+    item.classList.toggle("checked");
 }
