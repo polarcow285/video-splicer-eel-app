241901c920c1134b7a8817121f42adba244ca84d
Natalie Leung <mmoopal@gmail.com>
2024-08-26 21:24:44 -0400

    Trying to implement grouping functionality

--- web/script.js
+++ web/script.js
@@ -1,3 +1,6 @@
+//Map that stores groupName and list of indexes (paths)
+let groupMap = {}
+let currentEditingGroup = "";
 function get_path(){
     eel.get_path()(loadThumbnails)
 }
@@ -11,6 +14,7 @@ async function loadThumbnails() {
             img.src = thumb[0];
             img.className = 'thumbnail';
             img.dataset.index = thumb[1];
+            img.dataset.groupName = "";
             img.onclick = () => toggleSelection(img);
             container.appendChild(img);
         }
@@ -18,22 +22,62 @@ async function loadThumbnails() {
 }
 
 function toggleSelection(item){
-    item.classList.toggle("checked");
+    if (item.classList.contains("groupName") && isEditingCurrentGroup(item) == false){
+        alert("This clip is already part of a group!")
+        return;
+    }
+    item.classList.toggle("selected");
+}
+
+function isEditingCurrentGroup(clip){
+    if (clip.dataset.groupName == currentEditingGroup){
+        return true;
+    }
+    else{
+        return false;
+    }
+
 }
 
 function createGroup(event){
     event.preventDefault();
     let selectedClips = [];
+    let selectedElems = [];
     let thumbnails = document.querySelectorAll('.thumbnail');
     let groupName = document.getElementById('group_name_input').value;
-    thumbnails.forEach((thumbnail) => {
-        if (thumbnail.classList.contains('checked')) {
-            selectedClips.push(thumbnail.dataset.index);
+    if (groupName === '') {
+        alert("You must write something!");
+        return;
+      }
+    //for each thumbnail that is selected, store their paths, change its property to group, 
+    //and assign the groupName attribute to it
+    thumbnails.forEach((thumb) => {
+        if (thumb.classList.contains('selected')) {
+            selectedClips.push(thumb.dataset.index);
+            selectedElems.push(thumb)
+            thumb.classList.remove('selected');
+            thumb.classList.add('grouped');
+            thumb.classList.dataset.groupName = groupName
         }
     });
-    
-    // Now you can use selectedIndices for further processing
+    for (elem in selectedElem){
+        elem.classList.remove('selected');
+        thumb.classList.add('grouped');
+        thumb.classList.dataset.groupName = groupName
+    }
+
     console.log(selectedClips);
-    // You can send these indices to your Python backend using eel if needed
-    eel.create_group(selectedClips, groupName);
+    groupMap[groupName] = selectedClips
+    //eel.create_group(selectedClips, groupName);
+    newElement(groupName);
+}
+
+//Precondition: groupName is already ....
+function newElement(groupName) {
+    //new list item
+    var li = document.createElement("li");
+    var t = document.createTextNode(groupName);
+    li.appendChild(t);
+    document.getElementById("myUL").appendChild(li);
+
 }
